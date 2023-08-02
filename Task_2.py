2da43afe0aea3889a8c747f247690c0646efe12e
Тимур Ибатулин <timuribatulin@iMac-Timur-2.Dlink>
2023-08-02 12:49:00 +0300

    Вторая  задач_2а

--- Task_2.py
+++ Task_2.py
@@ -7,5 +7,6 @@ name_man = ['Иван', 'Петр', 'Семен', 'Максим']
 salary = [10, 200, 300, 400]
 premium = ['10.25%', '11.74%', '70.45%', '56.44%']
 
-happy_worker = {name_1: int(sal) * (float(pre.replace('%', ''))/100)  for name_1 in name_man for sal in salary for pre in premium}
+happy_worker = {name_man[i]: int(salary[i]) * (float(premium[i].replace('%', ''))/100) for i in range(len(name_man))}
+# happy_worker = {name_1: int(sal) * (float(pre.replace('%', ''))/100) for sal in salary for pre in premium for name_1 in name_man }
 print(happy_worker)
